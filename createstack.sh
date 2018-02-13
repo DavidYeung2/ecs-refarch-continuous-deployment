eb9212cdc20bf1ff5f081354a1747c010d21fc8c
David Williams <david.williams@polarseven.com>
2018-02-13 11:35:18 +1100

    bash script to create stack

---
+++ createstack.sh
@@ -0,0 +1,5 @@
+#!/bin/sh
+aws cloudformation create-stack --stack-name ECS-PIPELINE \
+--template-body file:///Users/davidwilliams/Projects/ecs-refarch-continuous-deployment/ecs-refarch-continuous-deployment.yaml \
+--parameters file:///Users/davidwilliams/Projects/ecs-refarch-continuous-deployment/params-master.json \
+--capabilities CAPABILITY_IAM
